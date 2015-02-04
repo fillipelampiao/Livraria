04007985d563cdeed24ff5fb38a6192feb448ec6
fillipelampiao <fillipe_santos@live.com>
2015-02-03 23:52:29 -0200

    Add classe Controller
    ...

---
+++ src/ch/makery/address/Main.java
@@ -0,0 +1,66 @@
+package ch.makery.address;
+
+import java.io.IOException;
+
+import ch.makery.address.Main;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	private BorderPane borde;
+	private Stage stage;
+	
+	private String secundaria = "view/Inicia.fxml";
+	private String primaria = "view/Acordion.fxml";
+	
+	@Override
+	public void start(Stage stage) {
+		this.stage = stage;
+		
+		iniciaAcordion(primaria);
+		
+		iniciaTelas(secundaria);
+		
+		
+	}
+	//Inicia tela acordion 
+    public void iniciaAcordion(String primaria) {
+        try {
+            // Carrega o root layout do arquivo fxml.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource(primaria));
+            borde = (BorderPane) loader.load();
+
+            // Mostra a scene (cena) contendo oroot layout.
+            Scene scene = new Scene(borde);
+            stage.setScene(scene);
+            stage.setResizable(false);     
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	public void iniciaTelas(String primaria){
+		try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource(secundaria));
+            AnchorPane pane = (AnchorPane) loader.load();
+
+            borde.setCenter(pane);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/ch/makery/address/controller/AcordionController.java
@@ -0,0 +1,156 @@
+package ch.makery.address.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TitledPane;
+
+public class AcordionController {
+
+    @FXML
+    private Button iniciaFuncionariosConsultaButtom;
+
+    @FXML
+    private Button iniciaFornecedoresConsultaButtom;
+
+    @FXML
+    private Button IniciaEstoqueRelatorioButtom;
+
+    @FXML
+    private Button iniciaClientesConsultaButtom;
+
+    @FXML
+    private Button iniciaPagamentoButtom;
+
+    @FXML
+    private TitledPane iniciaVendaButtom;
+
+    @FXML
+    private Button iniciaVenderButtom;
+
+    @FXML
+    private Button iniciaClientesCadastroButtom;
+
+    @FXML
+    private Button iniciaFuncionariosCadastroButtom;
+
+    @FXML
+    private Button iniciaAlteraVendaButtom;
+
+    @FXML
+    private TitledPane iniciaCadastrarButtom;
+
+    @FXML
+    private Button iniciaProdutosConsultaButtom;
+
+    @FXML
+    private Button iniciaGruposCadastroButtom;
+
+    @FXML
+    private Button iniciaFornecedoresCadastroButtom;
+
+    @FXML
+    private Button iniciaGruposConsultaButtom;
+
+    @FXML
+    private Button iniciaProdutosCadastroButtom;
+
+    @FXML
+    private Button IniciaVendasRelatorioButtom;
+
+    @FXML
+    void iniciaCadastrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaGruposCadastro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaProdutosCadastro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaFornecedoresCadastro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaClientesCadastro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaFuncionariosCadastro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaVenda(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaVender(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaPagamento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaAlteraVenda(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaConsulta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaGruposConsulta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaProdutosConsulta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaFornecedoresConsulta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaClientesConsulta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaFuncionariosConsulta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void iniciaRelatorio(ActionEvent event) {
+
+    }
+
+    @FXML
+    void IniciaVendasRelatorio(ActionEvent event) {
+
+    }
+
+    @FXML
+    void IniciaEstoqueRelatorio(ActionEvent event) {
+
+    }
+
+}
